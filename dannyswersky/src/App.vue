0148dbadcf47f65f9155c89a496d691a09a95ae8
Lauren Swersky <lauren.swersky@gmail.com>
2023-08-15 19:02:18 -0400

    styling for top part complete

--- dannyswersky/src/App.vue
+++ dannyswersky/src/App.vue
@@ -3,7 +3,9 @@ import Intro from './components/Intro.vue'
 </script>
 
 <template>
+  <div class="bg-gradient-to-b from-slate-700 to-transparent min-h-screen w-screen">
   <Intro />
+</div>
 </template>
 
 <style scoped>
--- dannyswersky/src/components/Intro.vue
+++ dannyswersky/src/components/Intro.vue
@@ -1,28 +1,40 @@
 <template>
-  <div>
-    <p className="text-green-800 text-3xl">Hello</p>
-  <span id="typedMessage" class="typed">{{ typed }}</span>
-  <img src="/pic.jpg">
 
-</div>
+  <div
+    class="h-screen w-screen flex flex-col-2  flex justify-start p-8"
+  >
+  <div class="mt-96 ml-24 text-6xl">
+    <span id="typedMessage" class="typed">{{ typed }}</span>
+    </div>
+  </div>
+  <div class="absolute top-64 right-20 ">
+      <img class="rounded-full h-96" src="/pic.jpg" />
+    </div>
+
 </template>
 
 <script>
-import Typed from 'typed.js';
-
+import Typed from "typed.js";
 
 export default {
- name: '',
- data() {
+  name: "",
+  data() {
     return {
-       typed: ''
+      typed: "",
     };
- },
- mounted() {
-    this.typed = new Typed('#typedMessage', {
-       strings: ['I am Daniel Swersky.', 'I am an educator.', 'I am a father.', 'I am a husband.', 'I am an environmentalist.', 'I am Daniel Swersky.'],
-       typeSpeed: 50,
+  },
+  mounted() {
+    this.typed = new Typed("#typedMessage", {
+      strings: [
+        "Hi, I am Daniel Swersky.",
+        "Hi, I am an educator.",
+        "Hi, I am a father.",
+        "Hi, I am a husband.",
+        "Hi, I am an eco-activist.",
+        "Hi, I am Daniel Swersky.",
+      ],
+      typeSpeed: 50,
     });
- }
-}
+  },
+};
 </script>
