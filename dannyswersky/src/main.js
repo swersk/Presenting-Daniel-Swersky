5a4d8cf7ad6c804e39fe4bfb07fa14d981faac7a
Lauren Swersky <lauren.swersky@gmail.com>
2023-08-15 18:20:28 -0400

    .

--- dannyswersky/src/main.js
+++ dannyswersky/src/main.js
@@ -3,8 +3,9 @@ import './style.css'
 import App from './App.vue'
 import VueTypedJs  from 'vue-typed-js'
 import Particles from "vue3-particles";
+const app = createApp(App);
+app.use(Particles);
+app.mount('#app');
 
-createApp(App).use(Particles)
-createApp(App).mount('#app')
 
 
