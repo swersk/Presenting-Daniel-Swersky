446b9bf098df83a8ffb66487173d16d6cfd0f21d
Lauren Swersky <lauren.swersky@gmail.com>
2023-08-16 09:56:24 -0400

    particles work

--- dannyswersky/src/components/Intro.vue
+++ dannyswersky/src/components/Intro.vue
@@ -1,107 +1,102 @@
 <template>
-  <div class="particle-container h-screen w-screen flex flex-col-2 flex justify-start p-8">
+  <div class="h-screen w-screen flex flex-col-2 flex justify-start p-8">
     <div class="mt-96 ml-24 text-6xl">
       <span id="typedMessage" class="typed">{{ typed }}</span>
     </div>
     <div class="absolute top-64 right-20">
       <img class="rounded-full h-96" src="/pic.jpg" />
     </div>
-    <Particles
-      id="tsparticles"
-      :particlesInit="particlesInit"
-      :particlesLoaded="particlesLoaded"
-      :options="{
-        background: {
-          color: {
-            value: '#F25278'
-          }
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: 'push'
-            },
-            onHover: {
-              enable: true,
-              mode: 'repulse'
-            },
-            resize: true
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40
-            },
-            push: {
-              quantity: 4
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4
-            }
-          }
-        },
-        particles: {
-          color: {
-            value: '#ffffff'
-          },
-          links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1
-          },
-          collisions: {
-            enable: true
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outModes: 'bounce',
-            random: false,
-            speed: 6,
-            straight: false
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800
-            },
-            value: 80
-          },
-          opacity: {
-            value: 0.5
-          },
-          shape: {
-            type: 'circle'
-          },
-          size: {
-            random: true,
-            value: 3,
-            animation: {
-              enable: true,
-              speed: 1,
-              minimumValue: 0.1,
-              sync: true
-            }
-          }
-        },
-        detectRetina: true
-      }"
-    />
   </div>
-  <div class="content-on-top">
-    <span>TESTTT!!!!</span>
-  </div>
+  <div class="border-4 border-black">
+      <Particles
+        id="tsparticles-full"
+        :particlesInit="particlesInit"
+        :particlesLoaded="particlesLoaded"
+        :options="{
+                    background: {
+                        color: {
+                            value: '#0d47a1'
+                        }
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: 'push'
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: 'repulse'
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 400,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40
+                            },
+                            push: {
+                                quantity: 4
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: '#ffffff'
+                        },
+                        links: {
+                            color: '#ffffff',
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1
+                        },
+                        collisions: {
+                            enable: true
+                        },
+                        move: {
+                            direction: 'none',
+                            enable: true,
+                            outMode: 'bounce',
+                            random: false,
+                            speed: 6,
+                            straight: false
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800
+                            },
+                            value: 80
+                        },
+                        opacity: {
+                            value: 0.5
+                        },
+                        shape: {
+                            type: 'circle'
+                        },
+                        size: {
+                            random: true,
+                            value: 5
+                        }
+                    },
+                    detectRetina: true
+                }"
+      />
+    </div>
 </template>
 
 <script>
 import Typed from "typed.js";
+import { loadSlim } from "tsparticles-slim";
+
 
 export default {
   name: "Intro",
@@ -111,12 +106,13 @@ export default {
     };
   },
   methods: {
-    particlesInit(particles) {
-      console.log('particles init', particles);
+    async particlesInit(engine) {
+      console.log("particles init: ", engine);
+      await loadSlim(engine);
     },
     particlesLoaded(particles) {
-      console.log('particles loaded', particles);
-    }
+      console.log("particles loaded", particles);
+    },
   },
   mounted() {
     this.typed = new Typed("#typedMessage", {
@@ -130,6 +126,7 @@ export default {
       ],
       typeSpeed: 50,
     });
+
   },
 };
 </script>
